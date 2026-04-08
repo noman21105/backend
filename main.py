cfd96c38cc68e40f5fbf8b6f76d0f82a1eb4328b
unknown <mn1535949@gmail.com>
2026-04-09 04:02:37 +0500

    login

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ app = FastAPI(title="Voice Assistant API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
